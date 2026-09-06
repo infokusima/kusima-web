08d1e15f9c43e893f3e9421869ecdc1e5ea1ed79
infokusima <info.kusima@gmail.com>
2026-09-06 23:46:43 +0200

    Obnovenie cache pre jemnejsiu titulku

--- layout-master.js
+++ layout-master.js
@@ -19,7 +19,7 @@
 
   disableLegacy();
   ensureCss('kusima-layout-master','layout-v1.css?v=20260906-1');
-  ensureCss('kusima-mood','mood-v1.css?v=20260906-3');
+  ensureCss('kusima-mood','mood-v1.css?v=20260906-4');
   new MutationObserver(()=>disableLegacy()).observe(document.head,{childList:true});
 
   const nameday=document.querySelector('#home .nameday-label');
