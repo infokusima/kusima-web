aab6d34dd069b0174df5c1a20e82f0d5e821c227
infokusima <info.kusima@gmail.com>
2026-09-07 14:52:45 +0200

    Oprava cennika: mimoriadna schodza 40 EUR a administracia uveru

--- data/cennik.js
+++ data/cennik.js
@@ -1,5 +1,5 @@
 window.KUSIMA_CENNIK={
-  aktualizovane:'06.09.2026',
+  aktualizovane:'07.09.2026',
   poznamka:'Cenník vychádza z doterajšieho cenníka KUSIMA platného od roku 2024. Uvedené ceny sú orientačné; dohodnutá cena je súčasťou konkrétnej zmluvy o výkone správy a môže zohľadňovať ročnú mieru inflácie. KUSIMA s.r.o. nie je platiteľom DPH.',
   polozky:[
     {sluzba:'Správa bytu v bytovom dome od 20 do 50 bytov',cena:'od 6 € / mesiac',poznamka:'za byt'},
@@ -8,7 +8,8 @@ window.KUSIMA_CENNIK={
     {sluzba:'Správa domovej výmenníkovej stanice tepla bez obsluhy',cena:'od 40 € / mesiac',poznamka:''},
     {sluzba:'Servisné výjazdy a opravy vlastnými kapacitami správcu',cena:'15 € / hod.',poznamka:''},
     {sluzba:'Príprava a realizácia písomného hlasovania',cena:'30 €',poznamka:''},
-    {sluzba:'Zvolanie mimoriadnej schôdze',cena:'30 €',poznamka:''},
+    {sluzba:'Zvolanie mimoriadnej schôdze',cena:'40 €',poznamka:''},
+    {sluzba:'Administrácia bankového úveru',cena:'1,35 % zo sumy poskytnutého úveru',poznamka:'minimálne 200 €'},
     {sluzba:'Vystavenie potvrdenia pre kataster alebo banku',cena:'20 €',poznamka:''},
     {sluzba:'Vyhľadávanie dokumentov na žiadosť vlastníka v archíve',cena:'15 € / hod.',poznamka:''},
     {sluzba:'Hotovostné operácie v pokladni',cena:'neposkytujeme',poznamka:'aktuálne správca hotovostné operácie nerealizuje'}
